1493e617084289169629aa02cef4a666616e453e
hiroshikado <okonomiyakunarahiroimono@yahoo.co.jp>
2020-11-28 17:46:01 +0900

    calc-trying

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main(void){
-    printf("Hello World!\n");
+
+int main(void)
+{
+    printf("4 + 3 = %d\n", 4+3);
+    printf("16の2乗は、%d\n", 16 * 16);
+    printf("5 - 3 * 2 = %d\n", 5 - 3 * 2);
+    printf("(5-3) * 2 = %d\n", (5-3) * 2);
+    printf("10割る3は、%d\n", 10 / 3);
+    printf("10割る3は、%f\n", 10.0 / 3.0);
+    printf("10を3で割った余りは、%d\n", 10 % 3);
+     
     return 0;
 }
