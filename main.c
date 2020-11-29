4608e405e7374f2f792c522b1512a25748a9169f
hiroshikado <okonomiyakunarahiroimono@yahoo.co.jp>
2020-11-29 15:22:09 +0900

    sizeof-trying

--- main.c
+++ main.c
@@ -2,13 +2,7 @@
 
 int main(void)
 {
-    printf("4 + 3 = %d\n", 4+3);
-    printf("16の2乗は、%d\n", 16 * 16);
-    printf("5 - 3 * 2 = %d\n", 5 - 3 * 2);
-    printf("(5-3) * 2 = %d\n", (5-3) * 2);
-    printf("10割る3は、%d\n", 10 / 3);
-    printf("10割る3は、%f\n", 10.0 / 3.0);
-    printf("10を3で割った余りは、%d\n", 10 % 3);
-     
+    size_t size = sizeof(int);
+    printf( "%dバイト¥n", (int)(sizeof(long)));
     return 0;
 }
